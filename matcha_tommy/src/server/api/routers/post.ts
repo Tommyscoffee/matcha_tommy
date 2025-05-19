84f9f82bbafdd5bd8c7c969106237196e20dcd22
Tommy <saintyoungmen1124@gmail.com>
2025-05-20 01:09:21 +0900

    resolve error

--- matcha_tommy/src/server/api/routers/post.ts
+++ matcha_tommy/src/server/api/routers/post.ts
@@ -5,6 +5,7 @@ import {
 	protectedProcedure,
 	publicProcedure,
 } from "~/server/api/trpc";
+import { db } from "~/server/db";
 
 export const postRouter = createTRPCRouter({
 	hello: publicProcedure
@@ -38,4 +39,30 @@ export const postRouter = createTRPCRouter({
 	getSecretMessage: protectedProcedure.query(() => {
 		return "you can now see this secret message!";
 	}),
+	getUser: publicProcedure.query(async ()=> {
+        return db.user.findMany();
+    }),
+    createUser: publicProcedure.input(z.object({
+        email: z.string(),
+        password: z.string(),
+        username: z.string(),
+        firstName: z.string(),
+        lastName: z.string(),
+        gender: z.string(),
+        sexualPreference: z.string(),
+        biography: z.string(),
+    })).mutation(async ({input}) => {
+        return db.user.create({
+            data: {
+                email: input.email,
+                password: input.password,
+                username: input.username,
+                firstName: input.firstName,
+                lastName: input.lastName,
+                gender: input.gender,
+                sexualPreference: input.sexualPreference,
+                biography: input.biography,
+            }
+        })
+    })	
 });
--- matcha_tommy/src/server/api/trpc.ts
+++ matcha_tommy/src/server/api/trpc.ts
@@ -10,6 +10,8 @@
 import { TRPCError, initTRPC } from "@trpc/server";
 import superjson from "superjson";
 import { z, ZodError } from "zod";
+import { type CreateNextContextOptions } from "@trpc/server/adapters/next";
+import { type Session } from "next-auth";
 
 import { auth } from "~/server/auth";
 import { db } from "~/server/db";
@@ -57,7 +59,6 @@ const t = initTRPC.context<typeof createTRPCContext>().create({
 	},
 });
 
-
 /**
  * Create a server-side caller.
  *
@@ -77,34 +78,7 @@ export const createCallerFactory = t.createCallerFactory;
  *
  * @see https://trpc.io/docs/router
  */
-export const createTRPCRouter = t.router({
-	getUser: t.procedure.query(async ()=> {
-		return db.user.findMany();
-	}),
-	createUser: t.procedure.input(z.object({
-		email: z.string(),
-		password: z.string(),
-		username: z.string(),
-		firstName: z.string(),
-		lastName: z.string(),
-		gender: z.string(),
-		sexualPreference: z.string(),
-		biography: z.string(),
-	})).mutation(async ({input}) => {
-		return db.user.create({
-			data: {
-				email: input.email,
-				password: input.password,
-				username: input.username,
-				firstName: input.firstName,
-				lastName: input.lastName,
-				gender: input.gender,
-				sexualPreference: input.sexualPreference,
-				biography: input.biography,
-			}
-		})
-	})
-});
+export const createTRPCRouter = t.router;
 
 export type createTRPCRouter = typeof createTRPCRouter;
 
